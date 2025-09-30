65bfa221f2376686556fb56b7fd8bacabd36bb1e
Jash Patwa <patwajash24@gmail.com>
2025-09-30 13:19:22 +0530

    Question 2

---
+++ Q2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    int age;
+    cout<<"Enter your name:";
+    cin>>name;
+    cout<<"Enter your age:";
+    cin>>age;
+    cout<<"Yout name is:"<<name<<"\nYour age is:"<<age;
+}
