876989128e093d8b2fd144fe8de557357cf99056
Jash Patwa <patwajash24@gmail.com>
2025-10-04 12:45:24 +0530

    Question 3

---
+++ Q3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class rectangle{
+        int length,width;
+    public:
+        rectangle(int l,int w){
+            length = l,width = w;
+        }
+        int area(){
+            return length*width;
+        }
+};
+
+int main(){
+    int length, width;
+    cout<<"Enter length:";
+    cin>>length;
+    cout<<"Enter width:";
+    cin>>width;
+    cout<<"Calculate area using POP: "<<length*width<<endl;
+    rectangle rect(length,width);
+    cout<<"Calculated area using OOP: "<<rect.area();
+    return 0;
+}
