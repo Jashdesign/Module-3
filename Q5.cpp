df40507ff2675a80bc1b2d18770b7cac8baeecc9
Jash Patwa <patwajash24@gmail.com>
2025-10-04 13:07:08 +0530

    Question 5

---
+++ Q5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const float pi = 3.14;
+    int a = 15;
+    cout << "PI value which is constant : "<<pi<<endl;
+    cout << "Value of A which is not constant: "<<a<<endl;
+}
